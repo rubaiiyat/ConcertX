9a39837d03c9cf463abc96f7fc52a2489900ec2e
rubaiiyat <abirr1516@gmail.com>
2025-11-05 19:56:51 +0600

    add sponser

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Banner from "./Banner";
 import Slider from "./Slider";
+import SponsorSlider from "./SponsorSlider";
 
 const Home = () => {
   return (
     <div className="mt-10">
       <Banner></Banner>
       <Slider></Slider>
+      <SponsorSlider></SponsorSlider>
     </div>
   );
 };
---
+++ src/Components/Pages/Home/SponsorSlider.jsx
@@ -0,0 +1,73 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/autoplay";
+import { Autoplay } from "swiper/modules";
+import { FaSpotify, FaSoundcloud, FaApple, FaDeezer } from "react-icons/fa";
+import { SiBandsintown, SiYoutubemusic } from "react-icons/si";
+
+const sponsors = [
+  {
+    id: 1,
+    icon: <FaSpotify className="text-green-500" size={50} />,
+    name: "Spotify",
+  },
+  {
+    id: 2,
+    icon: <FaSoundcloud className="text-orange-500" size={50} />,
+    name: "SoundCloud",
+  },
+  {
+    id: 3,
+    icon: <FaApple className="text-[#FB2A41]" size={50} />,
+    name: "Apple Music",
+  },
+  {
+    id: 4,
+    icon: <FaDeezer className="text-purple-500" size={50} />,
+    name: "Deezer",
+  },
+  {
+    id: 5,
+    icon: <SiBandsintown className="text-accent" size={50} />,
+    name: "Bandsintown",
+  },
+  {
+    id: 6,
+    icon: <SiYoutubemusic className="text-red-500" size={50} />,
+    name: "Youtube Music",
+  },
+];
+
+const SponsorSlider = () => {
+  return (
+    <div
+      style={{ fontFamily: "'Bebas Neue', cursive" }}
+      className="w-full py-10 bg-base-200 shadow "
+    >
+      <h2 className="text-3xl font-bold text-center mb-8">Our Sponsors</h2>
+      <Swiper
+        slidesPerView={5}
+        spaceBetween={30}
+        loop={true}
+        speed={5000}
+        autoplay={{
+          delay: 0,
+          disableOnInteraction: false,
+          reverseDirection: false,
+        }}
+        modules={[Autoplay]}
+      >
+        {sponsors.map((sponsor) => (
+          <SwiperSlide key={sponsor.id}>
+            <div className="flex flex-col items-center justify-center gap-2">
+              <div className="">{sponsor.icon}</div>
+              <p className="font-medium">{sponsor.name}</p>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
+
+export default SponsorSlider;
